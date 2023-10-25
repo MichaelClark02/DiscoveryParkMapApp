9fa04d8ffbeed2d7207340009740f58f7edb73db
bemnet merkebu <67043682+bartheart@users.noreply.github.com>
2023-10-24 19:02:28 -0500

    Update Navigation.tsx

--- DiscoveryParkMapApp/Navigation.tsx
+++ DiscoveryParkMapApp/Navigation.tsx
@@ -1,12 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { View, StyleSheet } from 'react-native'
+
 import Preference from './src/screens/Preference'
 import ContactUs from './src/screens/ContactUs'
 import LandingPage from './src/screens/LandingPage'
 import Map from './src/screens/map'
-import { Entypo } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
+import Search from './src/screens/search'
 
 
 
@@ -14,48 +13,12 @@ const Tab = createBottomTabNavigator()
 
 function TabGroup() {
     return (
-        <Tab.Navigator
-            screenOptions={{
-                tabBarInactiveTintColor: '#707070',
-                tabBarActiveTintColor: "#00853E",
-            }}
-            >
-            <Tab.Screen 
-            name='LandingPage' 
-            component={LandingPage}
-            options={{
-                tabBarIcon: ({focused}) => (
-                    <Entypo name="home" size={24} color={focused ? "#00853E" : "#707070"}
-                     />
-                )
-            }}/>
-
-            <Tab.Screen 
-                name='Preferences' 
-                component={Preference}
-                options={{
-                    tabBarIcon: ({focused}) => (
-                        <Ionicons name="filter" size={24} color={focused ? "#00853E" : "#707070"} />
-                    )
-                }}/>
-
-            <Tab.Screen 
-                name='ContactUs' 
-                component={ContactUs}
-                options={{
-                    tabBarIcon: ({focused}) => (
-                    <Entypo name="phone" size={24} color={focused ? "#00853E" : "#707070"} />
-                    )
-                }}/>
-
-            <Tab.Screen 
-                name='Map' 
-                component={Map}
-                options={{
-                    tabBarIcon: ({focused}) => (
-                        <Entypo name="map" size={24} color={focused ? "#00853E" : "#707070"} />
-                    )
-                }}/>
+        <Tab.Navigator>
+            <Tab.Screen name='LandingPage' component={LandingPage} options={{ headerShown: false }}/>
+            <Tab.Screen name='Preference' component={Preference} options={{ headerShown: false }}/>
+            <Tab.Screen name='ContactUs' component={ContactUs} options={{ headerShown: false }}/>
+            <Tab.Screen name='Map' component={Map} options={{ headerShown: false }}/>
+            <Tab.Screen name='Search' component={Search} options={{ headerShown: false }}/>
         </Tab.Navigator>
     )
 }
@@ -67,3 +30,4 @@ export default function Navigation () {
         </NavigationContainer>
     )
 }
+
