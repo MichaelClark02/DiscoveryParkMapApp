de4becf4f268d366314d87df291eec889ac702bc
Michael Clark <michaelpclark03@gmail.com>
2023-10-24 21:28:48 -0500

    graph created

--- DiscoveryParkMapApp/src/components/graph.tsx
+++ DiscoveryParkMapApp/src/components/graph.tsx
@@ -10,3 +10,44 @@ var adjMatrix = [[ 0,-1,-1,-1,-1, -1 ,  2 , -1 ,  2],
                  [ 2,-1,-1,-1, 2,  4 , -1 , -1 ,  0],
                 ]
 
+
+function addEdge(adj, u : GraphNode,v: GraphNode,wt: number)
+{
+    adj[u.getIndex()][v.getIndex()].push([wt]);
+    return adj;   
+}
+
+
+
+
+interface Graph {
+    index: number;
+    longitude: number;
+    latitude: number;
+    name: string;
+    type: string;
+    reachable: boolean;
+    getIndex():number;
+}
+
+class GraphNode implements Graph {
+    index: number;
+    longitude: number;
+    latitude: number;
+    name: string;
+    type: string;
+    reachable: boolean;
+
+    constructor(longitude: number, latitude: number, name: string, type: string, reachable: boolean) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.name = name;
+        this.type = type;
+        this.reachable = reachable;
+    }
+    getIndex(){
+        return this.index;
+    }
+}
+
+
