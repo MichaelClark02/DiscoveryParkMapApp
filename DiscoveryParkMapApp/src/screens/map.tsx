f4517e34e031530c9befbd47e83e4a1abd13a4d0
bemnet merkebu <67043682+bartheart@users.noreply.github.com>
2023-11-15 16:28:32 -0600

    integrate the custom map and navigation route drawings

--- DiscoveryParkMapApp/src/screens/map.tsx
+++ DiscoveryParkMapApp/src/screens/map.tsx
@@ -6,7 +6,8 @@ import { Component, useState, useEffect } from 'react';
 import * as Location from 'expo-location';
 
 
-import { LatLng, dBlock, bBlock, outline } from '../components/Floor1';
+import { LatLng, dWing, bWing, aWing, outline } from '../components/Floor1';
+import graphData from '../components/Graph_test';
 import HomeSearch from '../components/HomeSearch';
 
 
@@ -19,6 +20,7 @@ const topLeftOverlay:Coordinate = [
   33.2517800, -97.155390
 ]
 
+const { route, nodes } = graphData;
 
 var floor1 = true;
 var op = 0.4;
@@ -31,45 +33,25 @@ export default function Map()  {
   const [error, setError] = useState(null);
   const [lat, setLat] = useState(0);
   const [lon, setLong] = useState(0);
-  // useEffect(() => {
-    
-    // const getLocation = async () => {
-    //   let { status } = await Location.requestForegroundPermissionsAsync();
-    //   if (status !== "granted") {
-    //     console.log("Permission to access location was denied");
-    //     return;
-    //   }
 
-    //   let location = await Location.getCurrentPositionAsync({});
-    //   setCurrentLocation(location.coords);
+  const [shortestRoute, setShortestRoute] = useState([]);
+  const [polylineCoordinates, setPolylineCoordinates] = useState([]);
 
-    //   setInitialRegion({
-    //     latitude: location.coords.latitude,
-    //     longitude: location.coords.longitude,
-    //     latitudeDelta: 0.005,
-    //     longitudeDelta: 0.005,
-    //   });
-    // };
-  //   Location.watchPositionAsync(
-  //     {
-  //       accuracy: Location.Accuracy.High,
-  //       timeInterval: 10, // Update location every second (adjust as needed)
-  //     },
-  //     (location) => {
-  //       const newLocation = {
-  //         latitude: location.coords.latitude,
-  //         longitude: location.coords.longitude,
-  //       };
-  //       setCurrentLocation(newLocation);
-  //     }
-  //   );
-  // };
+  const calculateShortestRoute = () => {
+    const startNode = 'a'; // Update with your desired starting node
+    const endNode = 'g'; // Update with your desired ending node
 
+    const path = route.path(startNode, endNode, { cost: true });
 
+    // Ensure the path is continuous
+    const continuousPath = [startNode, ...path.path];
 
-  //   getLocation();
-  // }, []);
+    setShortestRoute(continuousPath);
+    setPolylineCoordinates(path.coordinates); // Set the polyline coordinates
+  };
+
   useEffect(()=> {
+    calculateShortestRoute();
     ;(async() => {
       let { status } = await Location.requestForegroundPermissionsAsync()
       if (status !== 'granted') {
@@ -95,79 +77,108 @@ export default function Map()  {
       };
       return (
         <SafeAreaView>
-          <MapView style={styles.map}
-          showsBuildings
-          showsUserLocation
-          followsUserLocation
-          provider={PROVIDER_GOOGLE}
-          userLocationPriority="high"
-          initialRegion={{
-            latitude: 33.25405149775475,
-            longitude: -97.15271196603254,
-            latitudeDelta: 0.0025,
-            longitudeDelta: 0.0025,
-          }}
-          mapType="hybrid"
-          rotateEnabled
-          onLongPress={handleMapLongPress} // Handle long press on the map
-          >
-            <Overlay
-                bounds = {[topLeftOverlay,bottomRightOverlay]}
-                image ={{ uri: 'https://preview.redd.it/h12geouyt0zb1.png?width=640&crop=smart&auto=webp&s=211838092890ac1a247293645cc70af34a8e06d'}}
-                opacity={1}
-                //bearing={0}
-                //tappable={false}
+    <MapView
+      style={styles.map}
+      showsBuildings
+      showsUserLocation
+      followsUserLocation
+      provider={PROVIDER_GOOGLE}
+      userLocationPriority="high"
+      initialRegion={{
+        latitude: 33.25405149775475,
+        longitude: -97.15271196603254,
+        latitudeDelta: 0.0025,
+        longitudeDelta: 0.0025,
+      }}
+      mapType="hybrid"
+      rotateEnabled
+      onLongPress={handleMapLongPress} // Handle long press on the map
+    >
+      <Overlay
+        bounds={[topLeftOverlay, bottomRightOverlay]}
+        image={{
+          uri: 'https://preview.redd.it/h12geouyt0zb1.png?width=640&crop=smart&auto=webp&s=211838092890ac1a247293645cc70af34a8e06d',
+        }}
+        opacity={1}
+        //bearing={0}
+        //tappable={false}
+      />
 
-            />
-            <Polyline
-              coordinates={outline.map((location: LatLng) => ({
-                latitude: location.latitude,
-                longitude: location.longitude,
-              }))}
-              strokeColor="#FF0000"
-              strokeWidth={6}
-            />
+      {shortestRoute.map((nodeName, index) => {
+        const node = nodes.find((n) => n.getName() === nodeName) || { latitude: 0, longitude: 0 };
+        const nextNode = nodes.find((n) => n.getName() === shortestRoute[index + 1]);
 
-            <Polyline
-              coordinates={dBlock.map((location: LatLng) => ({
-                latitude: location.latitude,
-                longitude: location.longitude,
-              }))}
-              strokeColor="green"
-              strokeWidth={6}
-            />
+        if (!nextNode) {
+          // Stop rendering polylines if nextNode is not found
+          return null;
+        }
+      
+        const nodeCoordinates = { latitude: node.latitude, longitude: node.longitude };
+        const nextNodeCoordinates = { latitude: nextNode.latitude, longitude: nextNode.longitude };
+      
+        // Check if any coordinate is 0, if yes, return null to avoid rendering
+        if (nodeCoordinates.latitude === 0 || nodeCoordinates.longitude === 0 ||
+            nextNodeCoordinates.latitude === 0 || nextNodeCoordinates.longitude === 0) {
+          return null;
+        }
+        
+        return (
+          <Polyline
+            key={`polyline-${index}`}
+            coordinates={[nodeCoordinates, nextNodeCoordinates]}
+            strokeColor="blue"
+            strokeWidth={4}
+          />
+        );
+      })}
 
-            <Polyline
-              coordinates={bBlock.map((location: LatLng) => ({
-                latitude: location.latitude,
-                longitude: location.longitude,
-              }))}
-              strokeColor="green"
-              strokeWidth={6}
-            />
-            
-            <Marker
-                coordinate={{latitude: 33.254806348852334, 
-                longitude: -97.153712485778}}
-                title={"K130"}
-                description={"Room"}
-            />
-           
-            {/* {currentLocation && (
-            <Marker
-              coordinate={{
-                latitude: currentLocation.latitude,
-                longitude: currentLocation.longitude,
-              }}
-              title="Your Location"
-            />
-            )}         */}
-          </MapView>
+      <Polyline
+        coordinates={outline.map((location: LatLng) => ({
+          latitude: location.latitude,
+          longitude: location.longitude,
+        }))}
+        strokeColor="#FF0000"
+        strokeWidth={6}
+      />
 
-          <View style={styles.bottomBar}>
-            < HomeSearch/>
-          </View>
-        </SafeAreaView>
+      <Polyline
+        coordinates={dWing.map((location: LatLng) => ({
+          latitude: location.latitude,
+          longitude: location.longitude,
+        }))}
+        strokeColor="green"
+        strokeWidth={6}
+      />
+
+      <Polyline
+        coordinates={bWing.map((location: LatLng) => ({
+          latitude: location.latitude,
+          longitude: location.longitude,
+        }))}
+        strokeColor="green"
+        strokeWidth={4}
+      />
+
+      <Polyline
+        coordinates={aWing.map((location: LatLng) => ({
+          latitude: location.latitude,
+          longitude: location.longitude,
+        }))}
+        strokeColor="yellow"
+        strokeWidth={6}
+      />
+
+      <Marker
+        coordinate={{ latitude: 33.254806348852334, longitude: -97.153712485778 }}
+        title={"K130"}
+        description={"Room"}
+      />
+    </MapView>
+
+    <View style={styles.bottomBar}>
+      <HomeSearch />
+    </View>
+  </SafeAreaView>
     )
 }
 
